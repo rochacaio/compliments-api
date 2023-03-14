8ec135a0f08cde723b007e190eec8b3e55d1ddcf
caio <caiorocha@deltaglobal.com.br>
2023-03-13 21:15:12 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  roots: ['<rootDIr>/src'],
+
+  collectCoverage: true,
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // The test environment that will be used for testing
+  testEnvironment: "jest-environment-node",
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
