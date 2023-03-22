8a1df0e6ce7b773293c121e99dce3235314c66c9
caio <caiorocha@deltaglobal.com.br>
2023-03-21 21:21:26 -0300

    feat: ensure SignUpController returns an error if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,10 @@
-import Any = jasmine.Any;
 
 export class SignUpController{
     handle(httpRequest: any): any
     {
         return {
-            statusCode: 400
+            statusCode: 400,
+            body: new Error('Missing param: name')
         };
     }
 }
